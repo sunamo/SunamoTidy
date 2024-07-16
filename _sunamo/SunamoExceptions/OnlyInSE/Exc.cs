fc82203b0eb1ca6a695261f1e78561482b031840
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-16 09:59:18 +0200

    before the second approach to remove *SunamoExceptions.cs

--- _sunamo/SunamoExceptions/OnlyInSE/Exc.cs
+++ _sunamo/SunamoExceptions/OnlyInSE/Exc.cs
@@ -2,28 +2,28 @@ namespace SunamoTidy._sunamo.SunamoExceptions.OnlyInSE;
 
 internal class Exc
 {
-    
-    
-    
-    
-    
+
+
+
+
+
     internal static bool aspnet = false;
     #region For easy copy in SunamoException project
     private static bool first = true;
     private static readonly StringBuilder sb = new();
-    
-    
+
+
     internal static string GetStackTrace(bool stopAtFirstSystem = false)
     {
         var r = GetStackTrace2(false, stopAtFirstSystem);
         return r.Item3;
     }
-    
-    
-    
-    
-    
-    internal static Tuple<string, string, string>  GetStackTrace2(
+
+
+
+
+
+    internal static Tuple<string, string, string> GetStackTrace2(
     bool fillAlsoFirstTwo = true,
     bool stopAtFirstSystem = false)
     {
@@ -33,7 +33,7 @@ internal class Exc
         StackTrace st = new();
         var v = st.ToString();
         var l = v.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToList();
-        
+
         l.RemoveAt(0);
         var i = 0;
         string type = null;
@@ -49,8 +49,8 @@ internal class Exc
                 }
             if (item.StartsWith("at System."))
             {
-                
-                
+
+
                 l.Add(string.Empty);
                 l.Add(string.Empty);
                 break;
@@ -58,35 +58,35 @@ internal class Exc
         }
         return new Tuple<string, string, string>(type, methodName, string.Join(Environment.NewLine, l));
     }
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
     internal static void TypeAndMethodName(string l, out string type, out string methodName)
     {
         var s2 = l.Split("at ")[1].Trim();
         var s = s2.Split("(")[0];
-        
-        
+
+
         var p = s.Split(new char[] { AllChars.dot }, StringSplitOptions.RemoveEmptyEntries).ToList();
         methodName = p[p.Count - 1];
         p.RemoveAt(p.Count - 1);
         type = string.Join(AllStrings.dot, p);
     }
     internal static bool _trimTestOnEnd = true;
-    
-    
-    
-    
+
+
+
+
     internal static string CallingMethod(int v = 1)
     {
         StackTrace stackTrace = new();
         var methodBase = stackTrace.GetFrame(v).GetMethod();
         var methodName = methodBase.Name;
-        if (_trimTestOnEnd) methodName = SHSunamoExceptions.TrimEnd(methodName, "Test");
+        if (_trimTestOnEnd) methodName = SH.TrimEnd(methodName, "Test");
         return methodName;
     }
     #region MyRegion
@@ -98,11 +98,11 @@ internal class Exc
         if (dx != -1) st = st.Substring(dx);
         return st;
     }
-    
-    
-    
-    
-    
+
+
+
+
+
     private static List<string> GetLines(string v)
     {
         var l = v.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToList();
--- _sunamo/SunamoExceptions/_AddedToAllCsproj/SH.cs
+++ _sunamo/SunamoExceptions/_AddedToAllCsproj/SH.cs
@@ -1,6 +1,6 @@
 namespace SunamoTidy._sunamo.SunamoExceptions._AddedToAllCsproj;
 
-internal class SHSunamoExceptions
+internal class SH
 {
     internal static string JoinNL(List<string> l)
     {
